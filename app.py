7da8568e7325e983581706c9b71bc99605e0fa8c
Durgance <gdurgance@gmail.com>
2021-11-19 16:41:19 +0530

    not using open cv

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import numpy as np
 def prepare(file):
     #img_array=cv2.imread(filepath,cv2.IMREAD_COLOR)
     img_array=file/255
-    new_array=cv2.resize(img_array,(128,128))
-    return new_array.reshape(-1,128,128,3)
+    #new_array=cv2.resize(img_array,(128,128))
+    return img_array.reshape(-1,128,128,3)
 
 class_dict={'Tomato Bacterial spot': 0,
             'Tomato Early blight': 1,
@@ -30,6 +30,7 @@ def prediction_cls(prediction):
 @st.cache
 def load_image(image_file):
     img=Image.open(image_file)
+    img=img.resize((128,128))
     return img
     pass
 
